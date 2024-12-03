97340ee8dcea16dd273b6d10db3b4aaef5ce3e4e
Thomas Carré <100042304+ThomaSneazy@users.noreply.github.com>
2024-12-04 00:42:31 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './styles/style.css'
+import Lenis from '@studio-freight/lenis'
+import gsap from 'gsap'
+
+console.log(
+    '%c Dev by Thomas Carré\n' + 
+    '🌐 Website: https://carre.studio.com\n' +
+    '🐦 Twitter/X: https://x.com/ThomasCarre_' +
+    '📸 Instagram: https://www.instagram.com/carre__studio/\n' +
+    '💼 LinkedIn: https://www.linkedin.com/in/thomas-carre/\n' ,
+    'background-color: #0b0b0b; color: #8B9A46; font-size:10px; padding:6px 10px 6px; border-radius:4px; line-height: 1.5;'
+)
